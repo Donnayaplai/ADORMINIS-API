5742ddf3a5708455aac6bbc2c688d9aaf66a18ed
Donnaya <donnayawang@gmail.com>
2021-06-13 13:38:04 +0700

    [EDIT] Add package path, body-parser

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const path = require("path");
+const bodyParser = require("body-parser");
+const bcrypt = requre("bcrypt");
 app.use(express.json());
+app.use(bodyParser.json());
+app.use("/", express.static(path.join(__dirname, "static")));
 
 app.get("/", function (req, res) {
   res.redirect("https://adorminis.netlify.app/");
 });
+
+//Hashing the password
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
