2b08fcecb2fe57b8cc85ee0b0e6b2d38de8f886e
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-06-22 00:35:03 +0700

    [EDIT] index.js

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 const bodyParser = require("body-parser");
-const bcrypt = requre("bcrypt");
+// const bcrypt = requre("bcrypt");
+const roomRouter = require('./routes/room');
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "static")));
 
-app.get("/", function (req, res) {
-  res.redirect("https://adorminis.netlify.app/");
-});
+// app.get("/", function (req, res) {
+//   res.redirect("https://adorminis.netlify.app/");
+// });
+
+app.get("/",roomRouter);
 
 //Hashing the password
 app.listen(PORT, () => {
