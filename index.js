25af3d6541892af32af1a39a600ef543037673e3
Donnaya <donnayawang@gmail.com>
2021-07-10 14:30:11 +0700

    [EDIT] Route, index.js

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 const express = require("express");
-const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require("path");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-// const bcrypt = requre("bcrypt");
-const roomRouter = require('./routes/room');
+//const bcrypt = require("bcrypt");
+//const jwt = require("jsonwebtoken");
+const roomRouter = require("./routes/room");
 
+const db = require("./config/db");
+
+const app = express();
 app.use(express.json());
-app.use(bodyParser.json());
-app.use("/", express.static(path.join(__dirname, "static")));
+// app.use("/", express.static(path.join(__dirname, "static")));
+app.use(cors());
 
+//Request Parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Connecting to Frontend
 // app.get("/", function (req, res) {
 //   res.redirect("https://adorminis.netlify.app/");
 // });
 
-app.get("/",roomRouter);
+//Register
+app.post("/signup", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+
+  db.query(
+    `INSERT INTO MANAGER (EMAIL, PASSWORD, FNAME, LNAME) VALUES (?,?,?,?)`,
+    [email, password, fname, lname],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+//Router
+app.get("/", roomRouter);
 
 //Hashing the password
 app.listen(PORT, () => {
