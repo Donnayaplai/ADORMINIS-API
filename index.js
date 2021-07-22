a9478849af05e90ce77e7864ad70ef174e0490f8
Donnaya <donnayawang@gmail.com>
2021-07-22 21:55:18 +0700

    [EDIT] Index.js
    add route for register api

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-//const bcrypt = require("bcrypt");
-//const jwt = require("jsonwebtoken");
-const roomRouter = require("./routes/room");
 
-const db = require("./config/db");
+// require("dotenv").config();
+require("./models/resident");
+
+// const roomRouter = require("./routes/room");
+
+const api = require("./api");
+// const db = require("./config/db");
 
 const app = express();
-app.use(express.json());
-// app.use("/", express.static(path.join(__dirname, "static")));
-app.use(cors());
 
-//Request Parsing
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", api);
+//Router
+// app.get("/", roomRouter);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
 //Connecting to Frontend
 // app.get("/", function (req, res) {
 //   res.redirect("https://adorminis.netlify.app/");
 // });
-
-//Register
-app.post("/signup", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const fname = req.body.fname;
-  const lname = req.body.lname;
-
-  db.query(
-    `INSERT INTO MANAGER (EMAIL, PASSWORD, FNAME, LNAME) VALUES (?,?,?,?)`,
-    [email, password, fname, lname],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Values Inserted");
-      }
-    }
-  );
-});
-
-//Router
-app.get("/", roomRouter);
-
-//Hashing the password
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
