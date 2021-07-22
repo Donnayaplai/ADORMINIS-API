b578ee7004d2b6010e29cd06a97534eb6c65b987
Donnaya <donnayawang@gmail.com>
2021-07-22 21:58:44 +0700

    [EDIT] database connection
    add sequelize.sync()

--- config/db.js
+++ config/db.js
@@ -1,15 +1,23 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('adorminis', 'admin', 'adorminis', {
-    host: 'adorminis.cp1aphjaifpb.ap-southeast-1.rds.amazonaws.com',
-    dialect: 'mariadb'
-});
-
+const sequelize = new Sequelize(
+  "adorminis",
+  "admin",
+  "adorminis",
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASS,
+  {
+    host: "adorminis.cp1aphjaifpb.ap-southeast-1.rds.amazonaws.com",
+    dialect: "mariadb",
+  }
+);
+sequelize.sync();
 try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
 } catch (error) {
-    console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to the database:", error);
 }
 
 module.exports = sequelize;
