4bc0e5fbe5d4bafc86c4e5a7e935cfcd3cbda9e2
Donnaya <donnayawang@gmail.com>
2021-07-22 21:57:11 +0700

    [EDIT] Resident Model
    add not null in FNAME and LNAME

--- models/resident.js
+++ models/resident.js
@@ -1,42 +1,91 @@
-const { DataTypes, Model } = require('sequelize');
-const sequelize = require('../config/db');
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/db");
 
-class RESIDENT extends Model { }
+class RESIDENT extends Model {}
 
-RESIDENT.init({
+RESIDENT.init(
+  {
     RESIDENTID: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
     FNAME: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     LNAME: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     TELNO: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     GENDER: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     EMAIL: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     PASSWORD: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     PERSONALCODE: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING,
     },
     IDCARDNO: {
-        type: DataTypes.STRING
-    }    
-}, {
+      type: DataTypes.STRING,
+    },
+  },
+  {
     freezeTableName: true,
     timestamps: false,
     sequelize,
-    modelName: 'RESIDENT'
-});
+    modelName: "RESIDENT",
+  }
+);
 
 module.exports = RESIDENT;
+
+// const { DataTypes } = require("sequelize");
+// const sequelize = require("../config/db");
+
+// const RESIDENT = sequelize.define("RESIDENT", {
+//   RESIDENTID: {
+//     type: DataTypes.INTEGER,
+//     primaryKey: true,
+//   },
+//   FNAME: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   LNAME: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   TELNO: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   GENDER: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   EMAIL: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   PASSWORD: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   PERSONALCODE: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   IDCARDNO: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+// });
+
+// module.exports = RESIDENT;
