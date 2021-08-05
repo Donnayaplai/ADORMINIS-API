9c9141f01f19cab5ce60db1c48acefb72aeae91a
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-08-05 19:48:56 +0700

    [EDIT] residentInfo route

--- routes/residentInfo.js
+++ routes/residentInfo.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 const ResidentInfo = require("../controller/residentInfo");
 
-router.get("/", async (req, res) => {
-  const info = await ResidentInfo.getResidentInfo;
-  return res.json(info);
-});
-
-// router.get("/:roomID", async (req, res) => {
-//   const roomID = req.params.roomID;
-//   const info = await ResidentInfo.getResidentInfo(roomID);
-//   res.json(info);
+// router.get("/", async (req, res) => {
+//   const info = await ResidentInfo.getResidentInfo;
+//   return res.json(info);
 // });
+
+router.get("/:roomID", async (req, res) => {
+  const roomID = req.params.roomID;
+  const residentInfo = await ResidentInfo.RESIDENT_INFO(roomID);
+  // console.log(residentInfo)
+  res.json(residentInfo)
+});
 
 module.exports = router;
