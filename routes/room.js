7eba597347a830c387bb1af5f6618eb461f01ee7
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-08-10 17:03:03 +0700

    [EDIT] room routes
    Test PM: http://localhost:3001/room/100000003/130000001

--- routes/room.js
+++ routes/room.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { ROOM_FINDALL } = require("../controller/room");
-
-// router.get("/", async (req, res) => {
-//   const room = await ROOM_FINDALL();
-//   console.log(room, "route")
-//   res.send(room)
-// });
+const { RESIDENT_INFO } = require("../controller/residentInfo");
 
 router.get("/:dormID", async (req, res) => {
   const dormID = req.params.dormID;
   const room = await ROOM_FINDALL(dormID);
-  // console.log(room)
   res.json(room)
 });
+
+router.get("/:dormID/:roomID", async (req, res) => {
+  const roomID = req.params.roomID;
+  const residentInfo = await RESIDENT_INFO(roomID);
+  res.json(residentInfo)
+});
+
+// router.get("/:roomID", async (req, res) => {
+//   const roomID = req.params.roomID;
+//   const residentInfo = await ResidentInfo.RESIDENT_INFO(roomID);
+//   console.log(residentInfo)
+//   res.json(residentInfo)
+// });
 
 module.exports = router;
