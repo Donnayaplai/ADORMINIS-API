9c2b61d1b5051c90959caea1dace12487404ed27
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-09-05 22:06:21 +0700

    [EDIT] Resident info controller
    Test: 
    http://localhost:3001/api/dormitory/100000003/120000001/130000001

--- controller/residentInfo.js
+++ controller/residentInfo.js
@@ -1,26 +1,50 @@
 const { DataTypes } = require("Sequelize");
-const User = require('../models/user');
-const Rent = require('../models/rent')
 
-User.hasMany(Rent, {
-  foreignKey: 'USERID',
-  as: "RENT"
-})
+const db = require('../config/db');
 
 const RESIDENT_INFO = async (roomID) => {
-  const data = await User.findAll({
-    include: [{
-      model: Rent,
-      attributes: ['CHECKINDATE', 'CHECKOUTDATE', 'CONTRACTOFRENTID', 'ROOMID'],
-      as: "RENT",
-      where: {
-        ROOMID: roomID,
-        CHECKOUTDATE: null
-      }
-    }]
-  });
-  // console.log(data, "controller")
-  return data;
+  const residentInfo = await db.query(
+    `SELECT u.USERID , u.FNAME , u.LNAME , u.TELNO , u.GENDER , u.IDCARDNO , u.EMAIL , u.PERSONALCODE ,
+            r.RENTID , r.CHECKINDATE , r.CHECKOUTDATE , r.CONTRACTOFRENTID ,
+            r2.ROOMID , r2.ROOMNO ,r2.FLOOR , r2.BUILDINGID , r2.ROOMTYPEID , r2.STATUS 
+    FROM USER u JOIN RENT r 
+    ON u.USERID = r.USERID
+    JOIN ROOM r2
+    ON r.ROOMID = r2.ROOMID
+    WHERE r.ROOMID = ?
+    AND r.CHECKOUTDATE IS NULL`,
+    {
+      replacements: [roomID],
+      type: db.QueryTypes.SELECT
+    }
+  )
+  // console.log(residentInfo, "<<<residentInfo")
+  return residentInfo
 }
+
+
+// const User = require('../models/user');
+// const Rent = require('../models/rent')
+
+// User.hasMany(Rent, {
+//   foreignKey: 'USERID',
+//   as: "RENT"
+// })
+
+// const RESIDENT_INFO = async (roomID) => {
+//   const data = await User.findAll({
+//     include: [{
+//       model: Rent,
+//       attributes: ['CHECKINDATE', 'CHECKOUTDATE', 'CONTRACTOFRENTID', 'ROOMID'],
+//       as: "RENT",
+//       where: {
+//         ROOMID: roomID,
+//         CHECKOUTDATE: null
+//       }
+//     }]
+//   });
+//   // console.log(data, "controller")
+//   return data;
+// }
 
 module.exports = { RESIDENT_INFO }
