b4a2ff030e35f6120485e0b735f0413523423c09
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-09-23 20:14:04 +0700

    [EDIT] rent controller
    Update status when add new resident

--- controller/rent.js
+++ controller/rent.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("Sequelize");
 const User = require('../models/user');
 const Rents = require('../models/rent');
+const Room = require('../models/room');
 
 const USER_INFO = async (personalCode) => {
     const user = await User.findOne({
@@ -24,14 +25,34 @@ const getUserByCode = async (personalCode) => {
 
 const ADD_USER = async (req, res) => {
     // console.log(req.body, "<<<addUser")
+
+    const todayDate = new Date().toISOString().slice(0, 10);
+    const roomID = req.params.roomID;
+
     const rent = {
-        CHECKINDATE: req.body.checkInDate ? req.body.checkInDate : null,
-        CONTRACTOFRENTID: req.body.contractOfRentID ? req.body.contractOfRentID : null,
+        CHECKINDATE: todayDate,
+        // CONTRACTOFRENTID: req.body.contractOfRentID ? req.body.contractOfRentID : null,
         USERID: await getUserByCode(req.body.personalCode),
-        ROOMID: req.params.roomID
+        ROOMID: roomID
     };
-    
+
     Rents.create(rent)
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.log(err)
+            return {
+                message:
+                    err.message
+            }
+        });
+
+    Room.update({ STATUS: 0 }, {
+        where: {
+            ROOMID: roomID
+        }
+    })
         .then(data => {
             return data;
         })
