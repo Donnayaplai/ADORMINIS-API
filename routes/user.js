36e7971f2e4ae75e5f44c685bc5104d276846721
Boonny <supawich.spv@mail.kmutt.ac.th>
2021-09-23 22:14:38 +0700

    [EDIT] user router

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-const { FIND_EMAIL, USER_REGISTER } = require('../controller/user');
+const { USER_REGISTER } = require('../controller/user');
 
 // @desc Register user
 // @access Public
@@ -20,58 +20,67 @@ router.post(
       'Please enter a password with with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    //See if user exists
-    const { fname, lname, telno, gender, IDCardNo, email, password } = req.body;
-    try {
-      let user = await FIND_EMAIL(email);
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exist' }] });
-      }
-
-      user = new User({
-        fname,
-        lname,
-        telno,
-        gender,
-        IDCardNo,
-        email,
-        password,
-      });
-      console.log(user, "<<<user")
-      //Encrypt password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      //Return jsonwebtoken
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      jwt.sign(
-        payload,
-        config.get('jwtSecret'),
-        { expiresIn: 360000000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.log(err.message);
-      res.status(500).send('Server error');
-    }
-  }
+  async (req, res) => { res.json(await USER_REGISTER(req, res)) }
 );
-// router.post('/register', USER_REGISTER);
 
 module.exports = router;
+
+//   {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+//     //See if user exists
+//     const { fname, lname, telno, gender, IDCardNo, email, password } = req.body;
+//     try {
+//       let user = await User.findOne({
+//         attributes: ['EMAIL'],
+//         where: {
+//           EMAIL: email
+//         }
+//       });
+//       if (user) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: 'User already exist' }] });
+//       }
+
+//       user = new User({
+//         fname,
+//         lname,
+//         telno,
+//         gender,
+//         IDCardNo,
+//         email,
+//         password,
+//       });
+//       console.log(user, "<<<user")
+//       //Encrypt password
+//       const salt = await bcrypt.genSalt(10);
+//       user.password = await bcrypt.hash(password, salt);
+
+//       await user.save();
+
+//       //Return jsonwebtoken
+//       const payload = {
+//         user: {
+//           id: user.id,
+//         },
+//       };
+//       jwt.sign(
+//         payload,
+//         config.get('jwtSecret'),
+//         { expiresIn: 360000000 },
+//         (err, token) => {
+//           if (err) throw err;
+//           res.json({ token });
+//         }
+//       );
+//     } catch (err) {
+//       console.log(err.message);
+//       res.status(500).send('Server error');
+//     }
+//   }
+// );
+
+// router.post('/register', USER_REGISTER);
