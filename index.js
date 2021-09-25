6e6d8b290d2be00669619f17828bb0435c3d6742
Donnaya <donnayawang@gmail.com>
2021-09-25 18:22:00 +0700

    [EDIT] index file

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const sequelize = require('./config/db');
-
+const morgan = require('morgan');
 const app = express();
 
 app.use(cors());
-
-//Connect Database
-sequelize.sync();
+app.use(morgan('dev'));
 
 //Init Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'API Running',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'API Running',
+//   });
+// });
 
 //Define Route
 
 //Room
-// const roomRouter = require('./routes/room');
-// app.use('/api/dormitory', roomRouter);
-// app.get('/', roomRouter);
-// Test PM: http://localhost:3001/room/100000003
-// Resdent info: http://localhost:3001/room/100000003/130000001
-// Add resident: http://localhost:3001/room/100000003/130000001/0/addRes
+const roomRouter = require('./routes/room');
+app.use('/api/room', roomRouter);
+
+//building
+const buildingRouter = require('./routes/building');
+app.use('/api/building', buildingRouter);
 
 //User
-app.use('/api/users', require('./routes/user'));
-// app.use('/api/auth', require('./routes/auth'));
+const userRouter = require('./routes/user');
+app.use('/api/user', userRouter);
 
 //Dorm
 // const dormRouter = require('./routes/dorm');
