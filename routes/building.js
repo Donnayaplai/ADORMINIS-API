41949f13ae548c40b9e4a97407843ed813483240
Donnaya <donnayawang@gmail.com>
2021-09-26 15:41:28 +0700

    [ADD] Building route

---
+++ routes/building.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { FIND_BUILDINGS } = require('../controller/room');
+
+//Get all building by dormID
+router.get('/:dormID', async (req, res) => {
+  const dormID = req.params.dormID;
+  const buildings = await FIND_BUILDINGS(dormID);
+  res.json(buildings);
+});
