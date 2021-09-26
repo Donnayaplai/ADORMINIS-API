a9faf16022dbc57f9b212152c162d20c7726d9a2
Donnaya <donnayawang@gmail.com>
2021-09-26 15:44:06 +0700

    [EDIT] database connection using .env file

--- config/db.js
+++ config/db.js
@@ -1,23 +1,20 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-  "adorminis",
-  "admin",
-  "adorminis",
-  // process.env.DB_NAME,
-  // process.env.DB_USER,
-  // process.env.DB_PASS,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: "adorminis.cp1aphjaifpb.ap-southeast-1.rds.amazonaws.com",
-    dialect: "mariadb",
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   }
 );
 sequelize.sync();
 try {
   sequelize.authenticate();
-  console.log("Connection has been established successfully.");
+  console.log('Connection has been established successfully.');
 } catch (error) {
-  console.error("Unable to connect to the database:", error);
+  console.error('Unable to connect to the database:', error);
 }
 
 module.exports = sequelize;
