760a811683e026d7edda47a7f99dcf0e13a43df8
Donnaya <donnayawang@gmail.com>
2021-09-26 15:41:13 +0700

    [EDIT] User route

--- routes/user.js
+++ routes/user.js
@@ -1,86 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
-const { USER_REGISTER } = require('../controller/user');
+const { RESIDENT_INFO } = require('../controller/residentInfo');
+const userController = require('../controller/user');
 
-// @desc Register user
-// @access Public
-router.post(
-  '/register',
-  [
-    check('fname', 'Name is required').not().isEmpty(),
-    check('lname', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with with 6 or more characters'
-    ).isLength({ min: 6 }),
-  ],
-  async (req, res) => { res.json(await USER_REGISTER(req, res)) }
-);
+//Get resident info RoomTable page
+router.get('/info/:roomID', async (req, res) => {
+  const roomID = req.params.roomID;
+  const residentInfo = await RESIDENT_INFO(roomID);
+  res.json(residentInfo);
+});
+
+router.post('/register', userController.USER_REGISTER);
+
+// router.post('/login', userController.USER_LOGIN);
 
 module.exports = router;
-
-//   {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-//     //See if user exists
-//     const { fname, lname, telno, gender, IDCardNo, email, password } = req.body;
-//     try {
-//       let user = await User.findOne({
-//         attributes: ['EMAIL'],
-//         where: {
-//           EMAIL: email
-//         }
-//       });
-//       if (user) {
-//         return res
-//           .status(400)
-//           .json({ errors: [{ msg: 'User already exist' }] });
-//       }
-
-//       user = new User({
-//         fname,
-//         lname,
-//         telno,
-//         gender,
-//         IDCardNo,
-//         email,
-//         password,
-//       });
-//       console.log(user, "<<<user")
-//       //Encrypt password
-//       const salt = await bcrypt.genSalt(10);
-//       user.password = await bcrypt.hash(password, salt);
-
-//       await user.save();
-
-//       //Return jsonwebtoken
-//       const payload = {
-//         user: {
-//           id: user.id,
-//         },
-//       };
-//       jwt.sign(
-//         payload,
-//         config.get('jwtSecret'),
-//         { expiresIn: 360000000 },
-//         (err, token) => {
-//           if (err) throw err;
-//           res.json({ token });
-//         }
-//       );
-//     } catch (err) {
-//       console.log(err.message);
-//       res.status(500).send('Server error');
-//     }
-//   }
-// );
-
-// router.post('/register', USER_REGISTER);
